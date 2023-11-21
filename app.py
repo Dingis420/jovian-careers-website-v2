94f8e74c17098f2060d4b211a87bc8683f02e73d
cbonwick <27862563-cbonwick@users.noreply.replit.com>
2023-11-21 22:59:14 +0000

    added javascript functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #import flask, and render template function
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 #defining a job list for dynamic entry
@@ -34,6 +34,8 @@ JOBS = [
 @app.route("/")
 def hello_world():
   return render_template("home.html", jobs=JOBS, company_name="Jovian")
-
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
